2960bbb677eed171a0d1103f1788725f058a8ce7
tuanan <pantoan288@gmail.com>
2024-11-19 15:50:38 +0700

    Fix

--- src/views/pages/PortfolioDetails.jsx
+++ src/views/pages/PortfolioDetails.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { portfolioData } from '../../data/portfolioData';
 import { motion } from 'framer-motion';
 
